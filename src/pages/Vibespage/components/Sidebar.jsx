030983966efc50ef56afab6b59ab3949d9e0b184
Li Blissz <liblissz3@gmail.com>
2025-12-21 22:39:06 +0100

    kmgk

--- src/pages/Vibespage/components/Sidebar.jsx
+++ src/pages/Vibespage/components/Sidebar.jsx
@@ -45,7 +45,7 @@ const Sidebar = () => {
       </div>
 
       <div className="overflow-y-auto w-full py-3">
-        {filteredUsers.map((user) => (
+        {filteredUsers?.map((user) => (
           <button
             key={user._id}
             onClick={() => setSelectedUser(user)}
